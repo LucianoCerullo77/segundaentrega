903b3ceca64684bcab59521ad5d9a279d6e9a33a
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-10-26 13:40:40 -0300

    nginx terminado

--- routes/extra/routeRandoms.js
+++ routes/extra/routeRandoms.js
@@ -2,17 +2,22 @@ const express = require("express");
 const { Router } = express;
 const minimist = require("minimist");
 
+const processors = require("os");
+
 const randomsRouter = new Router();
 
 
 
 randomsRouter.get("/", (req, res) => {
   console.log(`port: ${Object.values(minimist(process.argv.slice(2)))[0][0] || 8080} -> Fyh: ${Date.now()}`);
-  res.send(
-    `Servidor express <span style="color:blueviolet;">(Nginx)</span> en ${
-      Object.values(minimist(process.argv.slice(2)))[0][0] || 8080
-    } - <b>PID ${process.pid}</b> - ${new Date().toLocaleString()}`
-  );
+  
+  let info = {
+    port: Object.values(minimist(process.argv.slice(2)))[0][0] || 8080,
+    numb_random: Math.floor(Math.random() * 1000000 + 1),
+    processorsAmount: processors.cpus().length,
+  };
+  res.render("pages/randoms", { info: info });
+  
 });
 
 module.exports = randomsRouter;
